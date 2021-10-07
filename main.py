c6c32218dae6b9da2e407fa012d54d34a9fe3fac
Hafidz Naufal <91860668+hafidznaufl@users.noreply.github.com>
2021-10-07 12:10:40 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+nama = "Hafidz Naufal"
+umur = 17
+isMenikah = False
+print("Nama saya :", nama,
+"\nUmur Saya :", umur,
+"\nMenikah :", isMenikah)
+hobbies = ("olahraga", "ngoding", "design")
+print(hobbies[0])
+print(hobbies[1])
+print(hobbies[2])
+
+# Tipe Data
+dataString = "Hafidz Naufal"
+print("data :", dataString)
+print("-Bertipe", type(dataString))
+
+dataInterger = 12
+print("data :", dataInterger)
+print("-Bertipe", type(dataInterger))
+
+dataFloat = 1,3
+print("data :", dataFloat)
+print("-Bertipe", type(dataFloat))
+
+dataBool = True
+print("data :", dataBool)
+print("-Bertipe", type(dataBool))
+
+##MENGUHUK UHUK
